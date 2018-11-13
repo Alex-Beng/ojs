5a613439151cb74abd5bed8681c75a7370bda834
Alex_Beng <599267065@qq.com>
2018-11-13 17:24:04 +0800

    noj 1003 quick sort ac

---
+++ noj/1003_快速排序.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void quick_sort(int, int, int*);
+int partition(int, int, int, int*);
+
+void quick_sort(int head, int tail, int *num) {
+    if (head < tail) {
+        int pivot_point = head;
+        int next_pivot_point = partition(head, tail, pivot_point, num);
+        quick_sort(head, next_pivot_point-1, num);
+        quick_sort(next_pivot_point+1, tail, num);
+    } 
+}
+
+int partition(int head, int tail, int pivot_point, int *num) {
+    int pivot_num = num[pivot_point];
+    while (head < tail) {
+        while (head < tail && num[tail] >= pivot_num) tail--;
+        num[head] = num[tail];
+        while (head < tail && num[head] <= pivot_num) head++;
+        num[tail] = num[head];
+    }
+    num[head] = pivot_num;
+    // cout<<"pivot_point"<<head<<endl;
+    return head;
+}
+
+int main(int argc, char const *argv[]) {
+    int n;
+    cin>>n;
+    int num[n];
+    for (int i=0; i<n; i++) {
+        cin>>num[i];
+    }
+    quick_sort(0, n-1, num);
+    for (int i=0; i<n; i++) {
+        cout<<num[i]<<endl;
+    }
+    return 0;
+}
