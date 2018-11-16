20663965b9101b97b0f96cc3498df484c7cfc67f
Alex_Beng <599267065@qq.com>
2018-11-17 03:52:56 +0800

    update

--- noj/1148_merge_stones_dfs_with_memory_version.cpp
+++ noj/1148_merge_stones_dfs_with_memory_version.cpp
@@ -1,8 +1,10 @@
+#include <cstring>
 #include <iostream>
 using namespace std;
 
 int n;
 int stones_num[233];
+int dp[233][233];
 
 int get_sum(int i, int j) { // 从第i位开始的j个数之和
     int ans = 0;
@@ -13,24 +15,28 @@ int get_sum(int i, int j) { // 从第i位开始的j个数之和
 }
 
 int dfs(int begin, int end) { // 从第i位开始的j个堆中合并的最小值
+    if (dp[begin][end] != -1) {
+        return dp[begin][end];
+    }
     if (begin == end) {
         // cout<<"here"<<endl;
         return 0;
     }
     else if (begin+1 == end) {
-        return stones_num[begin] + stones_num[end];
+        return dp[begin][end] = (stones_num[begin] + stones_num[end]);
     }
     else {
         int res = 0x7fffffff;
         for (int k=begin; k<end; k++) { 
             res = min(res, dfs(begin,k)+dfs((k+1)%n,end)+get_sum(begin,end));
         }
-        return res;
+        return dp[begin][end] = res;
     }
 }
 
 int main(int argc, char const *argv[]) {
     while (cin>>n&&n) {
+        memset(dp, -1, sizeof(dp));
         for (int i=0; i<n; i++) {
             cin>>stones_num[i];
         }
