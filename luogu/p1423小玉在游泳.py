db776737cba4ad27ae67d9c9bb158bd5091d6d23
Alex Beng <599267065@qq.com>
2018-11-22 23:05:08 +0800

    luogu p1423 ac

--- luogu/p1423小玉在游泳.py
+++ luogu/p1423小玉在游泳.py
@@ -1,3 +1,12 @@
 from math import *
 distance = float(input())
-print(int(log(1-0.01*distance)/log(0.98) - 1))
+t_sum_distance = 0.0
+speed = 2.0
+ans = 0
+
+while t_sum_distance < distance:
+    t_sum_distance += speed
+    ans += 1 
+    speed *= 0.98
+
+print(ans)
