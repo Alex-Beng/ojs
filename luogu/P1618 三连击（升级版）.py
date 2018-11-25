39423a341fda36bfd1989a1b61165692ab9ce8c3
Alex Beng <599267065@qq.com>
2018-11-25 11:42:39 +0800

    luogu p1618 ac

--- luogu/P1618 三连击（升级版）.py
+++ luogu/P1618 三连击（升级版）.py
@@ -1,16 +1,30 @@
 a, b, c = map(int, input().split())
 
 digits = [0]*3
-used = [False]*10
 
 def check_ans():
     return
 
+flag = 1
 def dfs(layer):
+    global flag
     if layer == 3:
-        pass
+        t1 = int("".join([str(i) for i in digits]))
+        t2 = t1*b//a
+        t3 = t1*c//a
+        t_all = str(t1)+str(t2)+str(t3)
+        if len(t_all) > 9:
+            return
+        for i in range(1, 10):
+            if not str(i) in t_all:
+                return
+        print(t1, t2, t3)
+        flag = 0
     else:
-        pass
-    return
+        for i in range(1, 10):
+            digits[layer] = i
+            dfs(layer+1)
 
-dfs
+dfs(0)
+if flag:
+    print("No!!!")
