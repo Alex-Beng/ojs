6899a92a71f61a0ed4e4f368424745a7f35eca15
Alex-Beng <599267065@qq.com>
2019-01-26 13:56:48 +0800

    zju mooc self-test 2 ac

---
+++ FuckDataStructure/ZjuMooc/self-test_素数对猜想.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <cstring>
+#include <iostream>
+using namespace std;
+bool is_prime[100100]; 
+
+bool Is_Prime(int num, int flag) {
+    if (num==0 || num==1) {
+        return false;
+    }
+    if (flag == 0) { // 普通弱智判素数
+        for (int i=2; i*i<num; i++) {
+            if (num%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    else if (flag == 1) { // 埃式筛
+        // bool is_prime[num+1] = {true};
+        for (int i=2; i<=num ; i++) {
+            if (!is_prime[i]) {
+                for (int j=i*2; j<=num; j+=i) {
+                    is_prime[j] = true;
+                }
+            }
+        }
+        return is_prime[num];
+    }
+    
+}
+
+
+int main() {
+    is_prime[0] = true;
+    is_prime[1] = true;
+
+    int n;
+    cin>>n;
+    int counter = 0;
+    Is_Prime(n+10, 1);
+    // cout<<"here"<<endl;
+    
+    for (int i=2; i<=n; i++) {
+        if (!is_prime[i] && !is_prime[i-2]) {
+            // cout<<i<<' '<<i-2<<endl;
+            counter++;
+        } 
+    }
+    cout<<counter<<endl;
+    return 0;
+}
