644c1bcd6902691e406184b4d0d08ab0e13e8c1b
Alex_Beng <599267065@qq.com>
2019-03-06 22:00:18 +0800

    noj 1475 ac

---
+++ FuckDataStructure/Noj/Assignments/week_2/1475_rear和length表示的循环队列.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+template<class T>
+class Queue {
+private:
+    T* data;
+    int max_size;
+    int head;
+    int len;
+    bool resize(int new_size);
+public:
+    Queue(int size = 233);
+    ~Queue();
+    T Pop();
+    bool Push(T x);
+    T Back()const;
+    T Front()const;
+    bool Empty()const;
+    int Size()const;
+};
+
+template<class T>
+Queue<T>::Queue(int size) {
+    max_size = size;
+    data = new T[max_size];
+    if (data == NULL) {
+        cerr<<"Fail to new array in: "<<__FILE__<<" in: "<<__LINE__<<endl;
+    }
+
+    head = 0;
+    len = 0;
+}
+
+template<class T>
+Queue<T>::~Queue() {
+    delete[] data; 
+}
+
+template<class T>
+bool Queue<T>::resize(int new_size) {
+    T* new_data = new T[new_size];
+    if (new_data == NULL) {
+        cout<<"Fail to new array in: "<<__FILE__<<" in: "<<__LINE__<<endl;
+        return false;
+    }
+    for (int i=0; i<max_size; i++) {
+        new_data[i] = data[i];
+    }
+    delete[] data;
+    data = new_data;
+    return true;
+}
+
+template<class T>
+T Queue<T>::Pop() {
+    T t = data[head++];
+    head %= max_size;
+    len--;
+    return t;
+}
+
+template<class T>
+bool Queue<T>::Push(T x) {
+    if (len+1 <= max_size) {
+        data[(head+len++)%max_size] = x;
+    }
+    else {
+        return false;
+    }
+}
+
+template<class T>
+T Queue<T>::Back()const {
+    return data[(head+len-1)%max_size];
+}
+
+template<class T>
+T Queue<T>::Front()const {
+    return data[head];
+}
+
+template<class T>
+bool Queue<T>::Empty()const {
+    return (len==0);
+}
+
+template<class T>
+int Queue<T>::Size()const {
+    return len;
+}
+
+int main(int argc, char const *argv[]) {
+    int m;
+    int n;
+    int t;
+    cin>>n>>m;
+    Queue<int> qrq(m);
+    for (int i=0; i<n; i++) {
+        cin>>t;
+        if (t == 0) {
+            t = qrq.Pop();
+            cout<<t<<endl;
+        }
+        else {
+            if (qrq.Push(t)) {
+                ;
+            }
+            else {
+                cout<<"No"<<endl;
+            }
+        }
+    }
+    return 0;
+}
