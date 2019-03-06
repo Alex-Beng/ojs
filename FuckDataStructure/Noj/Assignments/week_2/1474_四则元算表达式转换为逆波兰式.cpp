db0520e08cb3f99f7fc6a1d74c673511ef95aa0d
Alex_Beng <599267065@qq.com>
2019-03-06 20:51:53 +0800

    noj 1474 ac

--- FuckDataStructure/Noj/Assignments/week_2/1474_四则元算表达式转换为逆波兰式.cpp
+++ FuckDataStructure/Noj/Assignments/week_2/1474_四则元算表达式转换为逆波兰式.cpp
@@ -97,7 +97,73 @@ int Stack<T>::Size()const {
     return max_size;
 }
 
+
+bool IsNum(char t) {
+    if (t == 'a' || t == 'b' || t == 'c') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool IsOpt(char t) {
+    if (t=='+' || t=='-' || t=='*' || t=='/') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+short Priority[256];
+
 int main(int argc, char const *argv[]) {
-    
+    // cout<<(int)'+'<<' '<<(int)'0'<<endl;
+    if (IsNum('+')) {
+        cout<<"Ya"<<endl;
+    }
+    Priority['('] = 1;
+    Priority[')'] = 1;
+    Priority['+'] = 2;
+    Priority['-'] = 2;
+    Priority['*'] = 3;
+    Priority['/'] = 3;
+
+    Stack<char> sk(233);
+    char str[233];
+    cin>>str;
+
+    string output = "";
+
+    for (int i=0; i<strlen(str); i++) {
+        if (IsNum(str[i])) {
+            // cout<<str[i]<<endl;
+            output.push_back(str[i]);
+        }
+        else if (IsOpt(str[i])) {
+            if (sk.Empty() || Priority[sk.Top()] < Priority[str[i]] ) {
+                sk.Push(str[i]);
+            }
+            else if (!sk.Empty() && Priority[sk.Top()] >= Priority[str[i]]) {
+                // cout<<str[i-1]<<' '<<str[i]<<endl;
+                output.push_back(sk.Pop());
+                sk.Push(str[i]);
+            }
+        }
+        else if (str[i] == '(') {
+            sk.Push(str[i]);
+        }
+        else if (str[i] == ')') {
+            while (sk.Top() != '(') {
+                output.push_back(sk.Pop());
+            }
+            sk.Pop();
+        }
+    }
+    while (!sk.Empty()) {
+        output.push_back(sk.Pop());
+    }
+    cout<<output<<endl;
     return 0;
 }
