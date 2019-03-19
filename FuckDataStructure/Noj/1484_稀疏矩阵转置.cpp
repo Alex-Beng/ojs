65cfd144e59c45ab84530789ccd0e7850db263b1
Alex_Beng <599267065@qq.com>
2019-03-19 10:44:12 +0800

    noj 1484 ac

--- FuckDataStructure/Noj/1484_稀疏矩阵转置.cpp
+++ FuckDataStructure/Noj/1484_稀疏矩阵转置.cpp
@@ -1,7 +1,5 @@
-#ifndef TRISPARSEMAT_H
-#define TRISPARSEMAT_H
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 template<class T>
@@ -9,6 +7,7 @@ struct Triple {
     int col;
     int row;
     T value;
+
     Triple() {
         col = -1;
         row = -1;
@@ -24,7 +23,7 @@ struct Triple {
 template<class T>
 bool for_sort(Triple<T>a, Triple<T>b) {
     if (a.row != b.row) {
-        return a.row > b.row;
+        return a.row < b.row;
     }
     else {
         return a.col < b.col;
@@ -68,7 +67,7 @@ void TripleSparseMat<T>::input() {
     cin>>row>>col;
 
     int t_r, t_c, t_v;
-    while (cin>>t_r>>t_c>>t_v && (t_r||t_c||t_c)) {
+    while (cin>>t_r>>t_c>>t_v && !(t_r==0&&t_c==0&&t_v==0)) {
         data[++value_nums].row = t_r;
         data[value_nums].col = t_c;
         data[value_nums].value = t_v;
@@ -94,4 +93,10 @@ void TripleSparseMat<T>::t() {
     sort(data, data+value_nums, for_sort<T>);
 }
 
-#endif 
+int main(int argc, char const *argv[]) {
+    TripleSparseMat<int> tsm;
+    tsm.input();
+    tsm.t();
+    tsm.output();
+    return 0;
+}
