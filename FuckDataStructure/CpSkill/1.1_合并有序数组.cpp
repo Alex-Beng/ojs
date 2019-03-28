a6b84abfca977989fb74edfd2ed2c329248debd6
Alex_Beng <599267065@qq.com>
2019-03-29 01:18:00 +0800

    cpskill assignment 1.1 done

--- FuckDataStructure/CpSkill/1.1_合并有序数组.cpp
+++ FuckDataStructure/CpSkill/1.1_合并有序数组.cpp
@@ -1,50 +1,83 @@
 #include <iostream>
 using namespace std;
 
+template<class T>
+class SeqList {
+protected:
+    T* data;
+    int max_size;
+    int tail;
+public:
+    SeqList(int size) {
+        if (size > 0) {
+            data = new T[size];
+            max_size = size;
+            tail = -1;
+        }
+    }
+
+    void PushBack(T x) {
+        if (tail+1 <= max_size-1) {
+            data[++tail] = x;
+        }
+    }
+    T getData(int idx) {
+        return data[idx];
+    }
+    int getLen() {
+        return tail+1;
+    }
+    void input() {
+        cin>>tail;
+        tail--;
+        for (int i=0; i<=tail; i++) {
+            cin>>data[i];
+        }
+    }
+    void output() {
+        for (int i=0; i<=tail; i++) {
+            cout<<data[i];
+            if (i != tail) {
+                cout<<endl;
+            }
+        }
+    }
+};
+
+template<class T>
+SeqList<T> Merge(SeqList<T> seq_list1, SeqList<T> seq_list2) {
+    int h1 = 0;
+    int h2 = 0;
+    int len1 = seq_list1.getLen();
+    int len2 = seq_list2.getLen();
+    SeqList<T> merged_list(len1+len2);
+    while (h1 < len1 && h2 < len2) {
+        if (seq_list1.getData(h1) <= seq_list2.getData(h2)) {
+            merged_list.PushBack(seq_list1.getData(h1++));
+        }
+        else {
+            merged_list.PushBack(seq_list2.getData(h2++));
+        }
+    }
+    
+    while(h1 < len1) {
+        merged_list.PushBack(seq_list1.getData(h1++));
+    }
+    while(h2 < len2) {
+        merged_list.PushBack(seq_list2.getData(h2++));
+    }
+    return merged_list;
+}
 
 int main(int argc, char const *argv[]) {
-    int nums[233], t_num, position_to_insert;
-    int len_1, len_2;
-    cin>>len_1;
-    
-    for (int i=0; i<len_1; i++) {
-        cin>>nums[i];
-    }
+    SeqList<int> sl_1(23);
+    SeqList<int> sl_2(23);
 
-    cin>>len_2;
-    for (int i=0; i<len_2; i++) {
-        cin>>t_num;
-        // do insertion
-        position_to_insert = 0;
-        while(true) {
-            if (position_to_insert == 0 && nums[0]>= t_num) {
-                break;
-            }
-            if (position_to_insert == len_1) {
-                break;
-            }
-            if (nums[position_to_insert] >= t_num && t_num > nums[position_to_insert-1]) {
-                break;
-            }
-            else {
-                position_to_insert++;
-            }
-        }
-        // cout<<t_num<<' '<<position_to_insert<<endl;
+    sl_1.input();
+    sl_2.input();
 
-        len_1++;
-        for (int k=len_1-1; k>=position_to_insert; k--) {
-            nums[k] = nums[k-1];
-        }
-        nums[position_to_insert] = t_num;
-    }  
-
-    for (int i=0; i<len_1; i++) {
-        cout<<nums[i];
-        if (i != len_1-1){
-            cout<<endl;
-        }
-        
-    }
+    SeqList<int> merged = Merge(sl_1, sl_2);
+    merged.output();
     return 0;
 }
+
