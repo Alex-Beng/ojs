46d61cf5281542521c22fdc01067f63234a45c82
Alex_Beng <599267065@qq.com>
2019-05-09 13:47:39 +0800

    huffman partly done & add experiment 3.1

--- FuckDataStructure/CppImplements/HuffmanTree.h
+++ FuckDataStructure/CppImplements/HuffmanTree.h
@@ -1,10 +1,38 @@
 #ifndef HUFFMAN_TREE_H
 #define HUFFMAN_TREE_H
+#include "MinHeap.h"
+
+#include <iostream>
+using namespace std;
 
 template<class T>
 struct TreeNode {
-    float data;
+    T data;
+    int weight;
+    TreeNode* l_child, r_child, parent;
+    TreeNode(): l_child(NULL), r_child(NULL), parent(NULL) {}
+    bool operator <=(TreeNode& r) {return this->weight <= r.weight;}
+    bool operator >(TreeNode& r) {return this->weight > r.weight;}
 };
 
+template<class T>
+class HuffmanTree {
+public:
+    HuffmanTree(TreeNode<T>* datas, int n);
+    ~HuffmanTree();
+protected:
+    TreeNode<T>* root;
+    string Encode(T a);
+    Decode(string str);
+};
+
+template<class T>
+HuffmanTree<T>::HuffmanTree(TreeNode<T>* datas, int n) {
+    MinHeap<int> hp(n);
+    
+    for (int i=0; i<n; i++) {
+        hp.Insert
+    }
+}
 
 #endif
