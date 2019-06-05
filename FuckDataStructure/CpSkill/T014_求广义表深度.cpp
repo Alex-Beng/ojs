65214b8666d952b0c48bf0b7d7abbce352f4812d
Alex_Beng <599267065@qq.com>
2019-06-05 20:48:58 +0800

    cpskill t014 wa

---
+++ FuckDataStructure/CpSkill/T014_求广义表深度.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    string input_str;
+    cin>>input_str;
+    cout<<input_str<<endl;
+
+    int t_depth = 0;
+    int max_depth = -1; 
+    
+    // 按表头分析
+    for (int i=0; i<input_str.length(); i++) {
+        if (input_str[i] == '(' 
+            && i+1<input_str.length() && input_str[i+1] != ')') {
+            t_depth++;
+        }
+        else if (input_str[i] == ')') {
+            t_depth--;
+        }
+        if (t_depth > max_depth) {
+            max_depth = t_depth;
+        }
+    }
+    cout<<t_depth<<endl<<t_depth;
+    return 0;
+}
