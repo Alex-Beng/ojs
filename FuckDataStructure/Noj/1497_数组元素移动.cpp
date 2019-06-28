8dadd04edb14fca0dd58001e129bde591d166b29
Alex_Beng <599267065@qq.com>
2019-06-28 22:43:28 +0800

    noj 1497 ac

--- FuckDataStructure/Noj/1497_数组元素移动.cpp
+++ FuckDataStructure/Noj/1497_数组元素移动.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char const *argv[]) {
     int n;
     int k;
+    int t;
     cin>>n;
     int nums[n];
     for (int i=0; i<n; i++) {
@@ -13,6 +14,16 @@ int main(int argc, char const *argv[]) {
 
     cin>>k;
     k %= n;
+    for (int i=0; i<k; i++) {
+        t = nums[n-1];
+        for (int i=n-1; i>=1; i--) {
+            nums[i] = nums[i-1];
+        }
+        nums[0] = t;
+    }
+    for (int i=0; i<n; i++) {
+        cout<<nums[i]<<endl;
+    }
     
     return 0;
 }
