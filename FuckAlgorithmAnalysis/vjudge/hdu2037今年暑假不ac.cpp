c6b7a1bd2aa7b6bd83a9d7117ff8d68a5f0b8f6b
Alex_Beng <599267065@qq.com>
2019-12-26 20:31:22 +0800

    hdu 2037 ac

--- FuckAlgorithmAnalysis/vjudge/hdu2037今年暑假不ac.cpp
+++ FuckAlgorithmAnalysis/vjudge/hdu2037今年暑假不ac.cpp
@@ -11,34 +11,30 @@ struct program {
 };
 
 bool for_sort(program p1, program p2) {
-    if (p1.end_time == p2.end_time) {
-        return p1.begin_time>p2.end_time;
-    }
-    else {
-        return p1.end_time<p2.end_time;
-    }
+    return p1.end_time<p2.end_time;
 }
 
 
 int main(int argc, char const *argv[]) {
     int n;
-    vector<program> pgs;
+    
     while (cin>>n && n) {
+        vector<program> pgs;
         for (int i=0; i<n; i++) {
             program t;
             cin>>t.begin_time>>t.end_time;
             pgs.push_back(t);
         }
-        sort(pgs.begin(), pgs.begin()+n, for_sort);
+        sort(pgs.begin(), pgs.end(), for_sort);
 #ifdef DEBUG
         for (unsigned int i=0; i<pgs.size(); i++) {
             cout<<pgs[i].begin_time<<' '<<pgs[i].end_time<<endl;
         }
 #endif  
         // get ans
-        int ans = 1;
-        int t_end = pgs[0].end_time;
-        for (unsigned int i=1; i<pgs.size(); i++) {
+        int ans = 0;
+        int t_end = -1;
+        for (unsigned int i=0; i<pgs.size(); i++) {
             if (pgs[i].begin_time >= t_end) {
                 t_end = pgs[i].end_time;
                 ans++;
