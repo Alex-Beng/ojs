31b59a3a1c09f349339186e24f70ff8dd9bb7f98
Alex_Beng <599267065@qq.com>
2019-12-26 21:29:31 +0800

    hdu 2044 ac

---
+++ FuckAlgorithmAnalysis/vjudge/hdu2044一只小蜜蜂....cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    long long int ans[66];
+    int n;
+    cin>>n;
+    for (int i=0; i<n; i++) {
+        int b, e;
+        cin>>b>>e;
+        ans[b] = 0;
+        ans[b+1] = 1;
+        ans[b+2] = 2;
+        for (int j=b+3; j<=e; j++) {
+            ans[j] = ans[j-1] + ans[j-2];
+        }
+        cout<<ans[e]<<endl;
+    }
+    return 0;
+}
