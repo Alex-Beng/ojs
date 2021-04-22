64ca5fa1ebbfdc634655c6091ea6abce0c649238
Alex_Beng <599267065@qq.com>
2021-04-23 00:14:01 +0800

    vj hdu 1096 ac, killing water problem...

---
+++ FuckAlgorithmAnalysis/vjudge/hdu1096.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int n;
+    cin>>n;
+    for (int i=0; i<n; i++) {
+        int t, sum=0, tt;
+        cin>>t;
+        for (int j=0; j<t; j++) {
+            cin>>tt;
+            sum += tt;
+        }
+        cout<<sum<<endl;
+        if (i!=n-1) cout<<endl;
+    }
+    return 0;
+}
