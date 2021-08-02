5515a6f59c74d5f64684a8f3732907bd8b2b6ecb
Alex Beng <599267065@qq.com>
2021-08-02 10:04:14 +0800

    leetcode 94. 二叉树的中序遍历 ac

---
+++ FuckLeetcode/94. 二叉树的中序遍历.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ans = []
+        def in_trav(node):
+            if node is None:
+                return
+            in_trav(node.left)
+            ans.append(node.val)
+            in_trav(node.right)
