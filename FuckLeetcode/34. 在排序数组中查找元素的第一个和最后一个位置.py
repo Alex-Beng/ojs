d6ad1c3c4e1affed325101158dab38792fb7c560
Alex_Beng <599267065@qq.com>
2021-08-16 22:44:38 +0800

    leetcode 34. 在排序数组中查找元素的第一个和最后一个位置 ac

---
+++ FuckLeetcode/34. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -0,0 +1,48 @@
+class Solution:
+    def searchRange(self, nums: [int], target: int) -> [int]:
+        n = len(nums)
+        if not n:
+            return [-1,-1]
+        Lb = Rb = -1
+
+        def valid(t):
+            if t>=0 and t<n:
+                return True
+            else:
+                return False
+
+        L = 0
+        R = n-1
+        while L<=R:
+            m = (L+R)//2
+            if nums[m]>target:
+                R = m-1
+            elif  nums[m]<=target:
+                L = m+1
+
+        print(L, R)
+        if valid(L) and nums[L] == target:
+            Rb = L
+        if valid(R) and nums[R] == target:
+            Rb = R
+        L = 0
+        R = n-1
+        while L<=R:
+            m = (L+R)//2
+            if nums[m]>=target:
+                R = m-1
+            elif  nums[m]<target:
+                L = m+1
+        print(L, R)
+        if valid(L) and nums[L] == target:
+            Lb = L
+        if valid(R) and nums[R] == target:
+            Lb = R
+        return [Lb, Rb]
+                
+
+s = Solution()
+print(s.searchRange(
+     [5,7,7,8,8,10], 8
+    # [1], 1
+))
