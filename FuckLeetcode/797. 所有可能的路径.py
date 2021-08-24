f6eed6863390e0a079ac005c6fc7b5b68100ab36
Alex Beng <599267065@qq.com>
2021-08-25 00:23:47 +0800

    leetcode 797. 所有可能的路径 5.27%

---
+++ FuckLeetcode/797. 所有可能的路径.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+class Solution:
+    def allPathsSourceTarget(self, graph: [[int]]) -> [[int]]:
+        n = len(graph)
+        ans = []
+
+        def dfs(curr_node, curr_path):
+            if curr_node == n-1:
+                ans.append(deepcopy(curr_path))
+                return 
+            
+            for to in graph[curr_node]:
+                curr_path.append(to)
+                dfs(to, curr_path)
+                del curr_path[-1]
+        dfs(0, [0])
+        # print(ans)
+        return ans
+s = Solution()
+s.allPathsSourceTarget(
+    #  [[1,2],[3],[3],[]]
+    [[4,3,1],[3,2,4],[3],[4],[]]
+)
