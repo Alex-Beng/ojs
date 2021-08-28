004a0d1453cbfc22e501404d5359762ecb18a362
Alex Beng <599267065@qq.com>
2021-08-29 02:00:21 +0800

    leetcode 104. 二叉树的最大深度 61.41%

--- FuckLeetcode/104. 二叉树的最大深度.py
+++ FuckLeetcode/104. 二叉树的最大深度.py
@@ -1,9 +1,4 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from util import *
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         ans = [0]
