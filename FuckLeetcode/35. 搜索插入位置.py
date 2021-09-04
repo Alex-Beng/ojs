772d6c2ff1f5637dd49000a57db44aaec7621db0
Alex Beng <599267065@qq.com>
2021-09-04 23:32:04 +0800

    leetcode 35. 搜索插入位置 74.72%

--- FuckLeetcode/35. 搜索插入位置.py
+++ FuckLeetcode/35. 搜索插入位置.py
@@ -7,16 +7,14 @@ class Solution:
         ans = n
         while L<=R:
             m = (L+R)//2
-            if nums[m]<target:
-                L = m+1
+            if target <= nums[m]:
                 ans = m
-            else:
                 R = m-1
-        # if 
-        return 0 if ans==n else ans+1
-        print(ans, L, R)
-        print(nums[ans])
+            else:
+                L = m+1
+        return ans
 s = Solution()
 s.searchInsert(
-    [1,3,5,6], 0
+    # [1,3,5,6], 0
+    [1,3,5,6], 7
 )
