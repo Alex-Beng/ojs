09c2799fa382463e06043a84067c3977477ca9d4
Alex_Beng <599267065@qq.com>
2021-11-04 10:44:52 +0800

    leetcode 908.最小差值-i 22.98%

---
+++ FuckLeetcode/908.最小差值-i.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=908 lang=python3
+#
+# [908] 最小差值 I
+#
+
+# @lc code=start
+class Solution:
+    def smallestRangeI(self, nums: [int], k: int) -> int:
+        return max(0, (max(nums)-min(nums))-2*k)
+# @lc code=end
