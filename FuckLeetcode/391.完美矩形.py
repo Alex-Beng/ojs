c4ec1e239b6c3aad65084c4874e2a696330f4e6f
Alex-Beng <599267065@qq.com>
2021-11-16 13:04:57 +0800

    may solve leetcode 391

--- FuckLeetcode/391.完美矩形.py
+++ FuckLeetcode/391.完美矩形.py
@@ -6,13 +6,19 @@
 
 # @lc code=start
 class Solution:
-    def isRectangleCover(self, rectangles: List[List[int]]) -> bool:
+    def isRectangleCover(self, rectangles: [[int]]) -> bool:
         xl = min([rect[0] for rect in rectangles])
         xr = max([rect[2] for rect in rectangles])
         yl = min([rect[1] for rect in rectangles])
-        yr = min([rect[3] for rect in rectangles])
-        
-        area_sum = sum([(rect[3]-rect[1])*(rect[2]-rect[0]) for rect in rectangles])
-        print(area_sum, (xr-xl)*(yr-yl))
+        yr = max([rect[3] for rect in rectangles])
+         
+        area_sum = sum([abs(rect[3]-rect[1])*abs(rect[2]-rect[0]) for rect in rectangles])
+        print([abs(rect[3]-rect[1])*abs(rect[2]-rect[0]) for rect in rectangles])
+        print(area_sum, abs(xr-xl)*abs(yr-yl))
         return area_sum == (xr-xl)*(yr-yl)
 # @lc code=end
+s = Solution()
+s.isRectangleCover(
+    [[1,1,3,3],[3,1,4,2],[3,2,4,4],[1,3,2,4],[2,3,3,4]]
+)
+
