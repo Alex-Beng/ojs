af0ab8b0f9e896ed9caaa092f07d0fa8a2257e11
Alex Beng <599267065@qq.com>
2023-02-06 20:34:49 +0800

    add make_tree & print_tree to util

--- FuckLeetcode/util.py
+++ FuckLeetcode/util.py
@@ -26,3 +26,36 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+def make_tree(assign: str):
+    assign = assign.replace('null', 'None')
+    vals = eval(assign.split('=')[-1])
+    def make_tree_helper(vals):
+        # 从层序遍历的数组构造二叉树
+        if len(vals) == 0:
+            return None
+        ya = []
+        for i in vals:
+            if i is None:
+                ya.append(None)
+            else:
+                ya.append(TreeNode(i))
+        for i in range(len(ya)):
+            if ya[i] is None:
+                continue
+            if 2*i+1 < len(ya):
+                ya[i].left = ya[2*i+1]
+            if 2*i+2 < len(ya):
+                ya[i].right = ya[2*i+2]
+        return ya[0]
+    return make_tree_helper(vals)
+
+def print_tree(root):
+    # 以树形输出二叉树
+    def print_tree_helper(root, depth):
+        if root is None:
+            return
+        print_tree_helper(root.right, depth+1)
+        print('    '*depth, root.val)
+        print_tree_helper(root.left, depth+1)
+    print_tree_helper(root, 0)
