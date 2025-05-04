e25e7eb3b243f6f2642ab2c6b2b76b14232fcd0f
Alex Beng <599267065@qq.com>
2025-05-04 22:06:36 +0800

    add reflection test

---
+++ FuckLeetcode/fuck_test.py
@@ -0,0 +1,55 @@
+# 命令行指定文件作为输入
+# 通过反射进行测试
+import sys
+import os
+import importlib
+from IPython import embed
+import inspect
+
+# author: 4o
+def import_module_from_path(relative_path, module_name):
+    # 获取当前文件的绝对路径
+    current_dir = os.path.dirname(__file__)
+    # 将相对路径转换为绝对路径
+    absolute_path = os.path.join(current_dir, relative_path)
+
+    # 使用importlib.util.spec_from_file_location来加载模块
+    spec = importlib.util.spec_from_file_location(module_name, absolute_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python fuck_test.py [code file] [func name]?")
+        exit(-1)
+    file_name = sys.argv[1]
+    print(file_name)
+    current_dir = os.path.dirname(__file__)
+
+    module = import_module_from_path(file_name, current_dir)
+    
+    sIns = module.Solution()
+    funcs = inspect.getmembers(sIns, predicate=inspect.ismethod)
+    # embed()
+    if len(funcs) != 1 and len(sys.argv) < 3:
+        print(f"Specify function name plz. in {funcs}")
+        exit(-2)
+    print(funcs)
+    func = funcs[0] if len(funcs) == 1 else getattr(sIns, sys.argv[2], None)
+    if not func:
+        print(f"func name not in {funcs}")
+        exit(-3)
+    
+    if not getattr(module, 'INPUT', None):
+        print("Specify INPUT in module plz.")
+        exit(-4)
+    
+    for i in module.INPUT:
+        r = func[1](*i)
+        print(f"{func[0]}{i} ==> {r}")
+    
+    # embed()
+    pass
