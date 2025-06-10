692b16abd41548471d5febd8a966b8966912e2d0
Alex Beng <599267065@qq.com>
2025-06-10 20:22:50 +0800

    leetcode 3442. 奇偶频次间的最大差值 I

--- FuckLeetcode/3442. 奇偶频次间的最大差值 I.py
+++ FuckLeetcode/3442. 奇偶频次间的最大差值 I.py
@@ -2,4 +2,10 @@ from collections import Counter
 class Solution:
     def maxDifference(self, s: str) -> int:
         c2n = Counter(s)
-        pass
+        odds = [n for n in c2n.values() if n&1]
+        evens = [n for n in c2n.values() if not n&1]
+
+        return max(odds)-min(evens)
+INPUT = [
+    ('tzt', )
+]
