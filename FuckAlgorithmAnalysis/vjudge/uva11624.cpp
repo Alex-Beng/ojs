8923a5f58df60861d17479b3c8e281c6f344e4a2
Alex Beng <599267065@qq.com>
2026-01-04 00:30:40 +0800

    vj uva11624 ac

--- FuckAlgorithmAnalysis/vjudge/uva11624.cpp
+++ FuckAlgorithmAnalysis/vjudge/uva11624.cpp
@@ -32,13 +32,13 @@ struct pair_hash {
 };
 
 
-int r, c;
-char maze[2333][3333];
-// int fire_step[2333][3333];
-std::unordered_map<coor, int, pair_hash> fire_step;
+int R, C;
+char MAZE[2333][3333];
+// int FIRE_STEP[2333][3333];
+std::unordered_map<coor, int, pair_hash> FIRE_STEP; // (R, C) -> step
 
 
-int diff[4][2] = {
+int DIFF[4][2] = {
     {0, 1},
     {0, -1},
     {1, 0},
@@ -46,118 +46,160 @@ int diff[4][2] = {
 };
 
 void out_fire_step() {
-    for (int i=0; i<r; i++) {
-        for (int j=0; j<c; j++) {
-            cout<<fire_step[make_pair(i, j)]<<' ';//!
+    cout<<"-----------fire step--------------"<<endl;
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
+            cout<<FIRE_STEP[make_pair(i, j)]<<' ';//!
         }
         cout<<endl;
     }
+    cout<<endl;
 }
 
 void out_joe_step(std::unordered_map<coor, int, pair_hash> joes_step) {
-    for (int i=0; i<r; i++) {
-        for (int j=0; j<c; j++) {
+    cout<<"-----------joes step--------------"<<endl;
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
             cout<<joes_step[make_pair(i, j)]<<' ';//!
         }
         cout<<endl;
     }
-
+    cout<<endl;
 }
 
-// 先bfs fire，获得fire step
-void bfs_fire(std::queue<coor> fire_qrq) {
+// bfs fire one step
+int bfs_fire(std::queue<coor> &fire_qrq) {
     coor t_co, t_n_co;
+    int curr_qrq_size = fire_qrq.size();
+    int cnt = 0;
+
+    // cout<<"fire start"<<endl;
     while (!fire_qrq.empty()) {
         t_co = fire_qrq.front();
         fire_qrq.pop();
+        cnt++;
         for (int i=0; i<4; i++) {
-            t_n_co.first = t_co.first + diff[i][0];
-            t_n_co.second = t_co.second + diff[i][1];
-
-            if (t_n_co.first>=0 && t_n_co.first<r
-            && t_n_co.second>=0 && t_n_co.second<c
-            && (maze[t_n_co.first][t_n_co.second] == '.' 
-               || maze[t_n_co.first][t_n_co.second] == 'J')
-            && fire_step[t_n_co] == 0) {//!
+            t_n_co.first = t_co.first + DIFF[i][0];
+            t_n_co.second = t_co.second + DIFF[i][1];
+            
+            // printf("%d %d fire why %d %d \n", t_n_co.first, t_n_co.second, (MAZE[t_n_co.first][t_n_co.second] == '.' 
+            //    || MAZE[t_n_co.first][t_n_co.second] == 'J'), FIRE_STEP[t_n_co] == 0);
+            if (t_n_co.first>=0 && t_n_co.first<R
+            && t_n_co.second>=0 && t_n_co.second<C
+            && (MAZE[t_n_co.first][t_n_co.second] == '.' 
+               || MAZE[t_n_co.first][t_n_co.second] == 'J')
+            // && FIRE_STEP[t_n_co] == 0) {//!
+            && FIRE_STEP.find(t_n_co) == FIRE_STEP.end()) {//!
                 fire_qrq.push(t_n_co);
-                fire_step[t_n_co] = fire_step[t_co] + 1;
+                FIRE_STEP[t_n_co] = FIRE_STEP[t_co] + 1;
             }
         }
+        if (cnt == curr_qrq_size) { // 只bfs一层
+            // cout<<"fire end"<<endl;
+            return -2;
+        }
     }
-    
+    return -1;
 }
 
-int bfs_joe(std::queue<coor> joes_qrq) {
-    std::unordered_map<coor, int, pair_hash> joes_step;
-
+// joe one step
+int bfs_joe(std::queue<coor> &joes_qrq, std::unordered_map<coor, int, pair_hash> &joes_step) {
     coor t_co, t_n_co;
-    t_co = joes_qrq.front();
-    joes_step[t_co] = 1;
+    // t_co = joes_qrq.front();
+    int curr_qrq_size = joes_qrq.size();
+    int cnt = 0;
+
+    // joes_step[t_co] = 1;
     while(!joes_qrq.empty()) {
         t_co = joes_qrq.front();
         joes_qrq.pop();
+        cnt++;
 
-        if (t_co.first==0 || t_co.first==r-1
-        || t_co.second==0 || t_co.second==c-1) {
+        if (t_co.first==0 || t_co.first==R-1
+        || t_co.second==0 || t_co.second==C-1) {
             // out_joe_step(joes_step);
             return joes_step[t_co];
         }
 
         for (int i=0; i<4; i++) {
-            t_n_co.first = t_co.first + diff[i][0];
-            t_n_co.second = t_co.second + diff[i][1];
-
-            if (t_n_co.first>=0 && t_n_co.first<r
-            && t_n_co.second>=0 && t_n_co.second<c
-            && (maze[t_n_co.first][t_n_co.second] == '.' 
-               || maze[t_n_co.first][t_n_co.second] == 'F')
+            t_n_co.first = t_co.first + DIFF[i][0];
+            t_n_co.second = t_co.second + DIFF[i][1];
+            
+            // printf("in %d %d v: %d\n", t_n_co.first, t_n_co.second, FIRE_STEP[t_n_co]);
+            // if (FIRE_STEP[t_n_co] != 0) {
+            if (FIRE_STEP.find(t_n_co) != FIRE_STEP.end()) {
+                // printf("worry in %d %d\n", t_n_co.first, t_n_co.second);
+                continue;
+            }
+            // printf("why %d %d %d %d %d\n", t_n_co.first>=0 && t_n_co.first<R, t_n_co.second>=0 && t_n_co.second<C, MAZE[t_n_co.first][t_n_co.second] == '.', joes_step[t_n_co] == 0, !FIRE_STEP[t_n_co]);
+            if (t_n_co.first>=0 && t_n_co.first<R
+            && t_n_co.second>=0 && t_n_co.second<C
+            &&
+             (MAZE[t_n_co.first][t_n_co.second] == '.')
             && joes_step[t_n_co] == 0
-            && joes_step[t_co]+1 < fire_step[t_n_co]) {
+            // && !FIRE_STEP[t_n_co] ) {
+            && FIRE_STEP.find(t_n_co) == FIRE_STEP.end() ) {
+                // cout<<"push: "<<t_n_co.first<<","<<t_n_co.second<<endl;
                 joes_qrq.push(t_n_co);
                 joes_step[t_n_co] = joes_step[t_co] + 1;
             }
+        }
+        if (cnt == curr_qrq_size) {
+            return -2;
         }
     }
     // out_joe_step(joes_step);
     return -1;
 }
 
+
 int main(int argc, char const *argv[]) {
     int n;
-    cin>>n;
+    // cin>>n;
+    scanf("%d", &n);
+    std::unordered_map<coor, int, pair_hash> joes_step;
     for (int ii=0; ii<n; ii++) {
-        fire_step.clear();
+        FIRE_STEP.clear();
+        joes_step.clear();
 
-        cin>>r>>c;
+        cin>>R>>C;
 
         std::queue<coor> fires;
         std::queue<coor> joes;// may not need 's' lol
-        for (int i=0; i<r; i++) {
-            for (int j=0; j<c; j++) {
-                cin>>maze[i][j];//!
-                if (maze[i][j] == 'F') {
-                    fire_step[make_pair(i, j)] = 1;
+        for (int i=0; i<R; i++) {
+            for (int j=0; j<C; j++) {
+                cin>>MAZE[i][j];//!
+                if (MAZE[i][j] == 'F') {
+                    FIRE_STEP[make_pair(i, j)] = 1;
                     fires.push(make_pair(i, j));
                 }
-                if (maze[i][j] == 'J') {
+                if (MAZE[i][j] == 'J') {
+                    joes_step[make_pair(i, j)] = 1;
                     joes.push(make_pair(i, j));
                 }
             }
         }
 
-        // out_fire_step();
-
-        bfs_fire(fires);
-
-        // out_fire_step();
-        int ans = bfs_joe(joes);
-
-        if (ans == -1) {
-            cout<<"IMPOSSIBLE"<<endl;
+        while (true) {
+            // out_fire_step();
+            bfs_fire(fires);
+            // out_fire_step();
+            // out_joe_step(joes_step);
+            int ans = bfs_joe(joes, joes_step);
+            // out_joe_step(joes_step);
+            if (ans == -1) {
+                cout<<"IMPOSSIBLE"<<endl;
+                break;    
+            }
+            else if (ans == -2) {
+                continue;
+            }
+            else {
+                cout<<ans<<endl;
+                break;
+            }
         }
-        else {
-            cout<<ans<<endl;
-        }
+        
     }
     return 0;
 }
